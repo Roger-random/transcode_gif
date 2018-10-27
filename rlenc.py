1986825d132b146d66f97838617991a0757c82c0
Roger Cheng <roger.random@outlook.com>
2018-10-26 20:45:17 -0700

    Single frame extraction

--- rlenc.py
+++ rlenc.py
@@ -21,3 +21,45 @@ for color in crop240.getcolors():
   palette = palette[3:] # This feels very LISP-y
 
 print("};")
+print()
+
+# Scan frame line-by-line, encode pixels via simple run-length encoding
+
+rlepixels = list()
+
+currentcolor = None
+currentcount = 0
+
+for y in range(240):
+  for x in range(320):
+    pixel = crop240.getpixel((x,y))
+    if pixel == currentcolor:
+      currentcount = currentcount + 1
+      if currentcount == 255:
+        rlepixels.append((currentcolor, currentcount))
+        currentcount = 0
+    else:
+      if currentcount > 0:
+        rlepixels.append((currentcolor, currentcount))
+      currentcolor = pixel
+      currentcount = 1
+  # End of line, flush any accumulated pixels.
+  if currentcount > 0:
+    rlepixels.append((currentcolor, currentcount))
+    currentcount = 0
+
+print("const uint16_t frame{}[{}] = {{".format(frame,len(rlepixels)))
+
+numperline = 0
+
+print("  ",end="")
+for encpixel in rlepixels:
+  print("0x{:02X}{:02X}, ".format(*encpixel),end="")
+  numperline = numperline + 1
+  if numperline >= 10:
+    print()
+    print("  ",end="")
+    numperline = 0
+
+print()
+print("};")
